eb9e572a95f0101c9d5f5c4d3547fc84bdbe9d10
Cindy Juarez <sceendy@gmail.com>
2018-01-17 19:38:03 -0600

    new bundle

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,10 +18,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in an anchor tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('a').textContent).toContain('ATX Paw Finder');
   }));
 });
